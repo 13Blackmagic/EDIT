bb27f62977f42d67390f4ef0077998ee4b79c864
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-19 19:26:15 -0600

    finished webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,6 +45,21 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+            exclude: /node_modules/,
+        },
+      },
         
       ],
     },
